774038e70ad4416f01a2f6a1c60f28364d02aabf
venkateshflujo <venkatesh@flujo.io>
2021-03-16 13:55:39 +0530

    promises

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PromisesComponent } from './promises/promises.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PromisesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
