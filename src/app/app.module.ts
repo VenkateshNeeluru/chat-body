f58dbfc7a5ded1cdf14d9b83107117ec5f56e27d
venkateshflujo <venkatesh@flujo.io>
2021-03-18 17:26:29 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PromisesComponent } from './promises/promises.component';
-import { ObservablesComponent } from './observables/observables.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PromisesComponent,
-    ObservablesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule,
-    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
